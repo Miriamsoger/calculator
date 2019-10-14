dd4f2362c2894a183d6e29bfdeab4babb917af1d
Miriamsoger <miriamsobri1995@gmail.com>
2019-10-14 15:00:55 +0200

    first commit

---
+++ javascript/javascript.js
@@ -0,0 +1,30 @@
+function init(){
+  var resultado= document.getElementById('resultado');
+  var clear = document.getElementById('clear');
+  var back = document.getElementById('back');
+  var suma = document.getElementById('suma');
+  var resta= document.getElementById('resta');
+  var multiplicacion = document.getElementById('multiplicacion');
+  var division = document.getElementById('division');
+  var punto= document.getElementById('punto');
+  var igual= document.getElementById('igual');
+  var cero= document.getElementById('cero');
+  
+  var dos= document.getElementById('dos');
+  var tres= document.getElementById('tres');
+  var cuatro= document.getElementById('cuatro');
+  var cinco= document.getElementById('cinco');
+  var seis= document.getElementById('seis');
+  var siete= document.getElementById('siete');
+  var ocho= document.getElementById('ocho');
+  var nueve= document.getElementById('nueve');
+
+
+}
+
+document.getElementById('uno').addEventListener("click", primero(){
+  alert("hola");
+});
+
+
+}
